2d694d061d2edc64e6f84e379c43786004b9a9dc
Ashley Kitson <ashley@zf4.biz>
2014-08-04 06:27:44 +0100

    add comparator

---
+++ src/chippyash/Math/Type/Comparator/ComparatorEngineInterface.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Arithmetic calculation support for chippyash Strong Types
+ *
+ * @author Ashley Kitson <akitson@zf4.biz>
+ * @copyright Ashley Kitson, UK, 2014
+ * @licence GPL V3 or later : http://www.gnu.org/licenses/gpl.html
+ */
+namespace chippyash\Math\Type\Comparator;
+
+use chippyash\Type\Number\NumericTypeInterface as NI;
+
+/**
+ * Defines an interface that type comparator engines must conform to
+ *
+ */
+interface ComparatorEngineInterface
+{
+    /**
+     * a == b = 0
+     * a < b = -1
+     * a > b = 1
+     *
+     * @param \chippyash\Type\Number\NumericTypeInterface $a
+     * @param \chippyash\Type\Number\NumericTypeInterface $b
+     *
+     * @return int
+     */
+    public function compare(NI $a, NI $b);
+
+    /**
+     * Convert float or int into relevant strong type
+     *
+     * @param numeric $num
+     * 
+     * @return \chippyash\Type\Number\NumericInterface
+     */
+    public function convertNumeric($num);
+}
